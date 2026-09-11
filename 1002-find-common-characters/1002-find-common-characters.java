9be8f1d7f6cdc4a9d15b83363768ef2f91de040e
Ravi Singh <ravi8959singh@gmail.com>
2026-09-11 11:26:07 +0530

    Time: 4 ms (43.05%), Space: 44.5 MB (81.45%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -2,39 +2,31 @@ import java.util.*;
 
 class Solution {
     public List<String> commonChars(String[] words) {
-        
         List<String> ans = new ArrayList<>();
-        
-        int[] minFreq = new int[26];
-        
-        // First word ki frequency
-        for(char ch : words[0].toCharArray()) {
-            minFreq[ch - 'a']++;
+       int comm[] = new int[26];
+       for(int j =0;j<words[0].length();j++){
+        char ch = words[0].charAt(j);
+        comm[ch-'a']++;
+       }
+       for(int i = 0;i<words.length;i++){
+        int curr[] = new int[26];
+        for(int j =0;j<words[i].length();j++){
+            char ch = words[i].charAt(j);
+            curr[ch -'a']++;
         }
-        
-        // Remaining words check karo
-        for(int i = 1; i < words.length; i++) {
-            
-            int[] freq = new int[26];
-            
-            for(char ch : words[i].toCharArray()) {
-                freq[ch - 'a']++;
-            }
-            
-            // Minimum frequency store karo
-            for(int j = 0; j < 26; j++) {
-                minFreq[j] = Math.min(minFreq[j], freq[j]);
-            }
+         for(int k =0;k<26;k++){
+        comm[k] = Math.min(comm[k],curr[k]); 
+       }
+       }
+      
+       for(int i = 0;i<26;i++){
+        while(comm[i]>0){
+            char ch = (char)(i+'a');
+            ans.add(String.valueOf(ch));
+            comm[i]--;
         }
-        
-        // Answer banao
-        for(int i = 0; i < 26; i++) {
-            while(minFreq[i] > 0) {
-                ans.add(String.valueOf((char)(i + 'a')));
-                minFreq[i]--;
-            }
-        }
-        
-        return ans;
+
+       }
+       return ans;
     }
 }
